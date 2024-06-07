8a905da9496aed190fd4bfd8bc8afdf454d3cc99
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-07 16:43:41 +0200

    Update Footer.jsx

--- public/src/Componentes/Footer/Footer.jsx
+++ public/src/Componentes/Footer/Footer.jsx
@@ -1,28 +1,20 @@
-import { Link } from 'react-router-dom';
-import { Box, Text, Flex, Stack, Divider, Link as ChakraLink } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Flex, Link, Text, Stack } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-        <Box as="footer" py={10} bg="gray.800" color="white">
-            <Stack spacing={8} align="center">
-                <Box textAlign="center">
-                    <Text>*Esto es solo un proyecto de desarrollo FullStack MERN.</Text>
-                    <Text>Comprar Pokémon va en contra de la filosofía de los creadores de Pokémon.</Text>
-                    <Text>Capturarlos te saldrá mejor.</Text>
-                </Box>
-                <Box textAlign="center">
-                    <Text>Proyecto para el bootcamp online de Full Stack de The Bridge.</Text>
-                    <Text>Realizado por Álvaro Martín y Adrián Canosa.</Text>
-                </Box>
-                <Divider borderColor="gray.600" />
-                <Flex justify="center">
-                    <ChakraLink as={Link} to="/about" mx={2} fontWeight="bold" color="blue.400">
-                        About
-                    </ChakraLink>
-                </Flex>
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box bg="gray.800" color="white" py={4}>
+      <Flex align="center" justify="center" direction="column">
+        <Stack direction="row" spacing={4}>
+          <Link as={RouterLink} to="/" color="white">Home</Link>
+          <Link as={RouterLink} to="/about" color="white">About</Link>
+          <Link as={RouterLink} to="/contact" color="white">Contact</Link>
+        </Stack>
+        <Text mt={2}>&copy; {new Date().getFullYear()} My Pokemon Store. All rights reserved.</Text>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Footer;
