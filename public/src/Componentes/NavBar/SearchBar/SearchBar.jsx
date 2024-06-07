b1254358b9320b94688a1e5faf938f99a1c8721a
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-07 08:16:10 +0200

    Update SearchBar.jsx

--- public/src/Componentes/NavBar/SearchBar/SearchBar.jsx
+++ public/src/Componentes/NavBar/SearchBar/SearchBar.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import { Box, Input, IconButton } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+  const [query, setQuery] = useState('');
   const [showInput, setShowInput] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (query) {
+      navigate(`/pokemon/${query}`);
+    }
+  };
 
   return (
     <Box textAlign="center">
       {showInput || window.innerWidth >= 768 ? (
-        <Input placeholder="Buscar..." width="200px" />
+        <form onSubmit={handleSearch}>
+          <Input
+            placeholder="Buscar..."
+            width="200px"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </form>
       ) : (
         <IconButton
           icon={<SearchIcon />}
