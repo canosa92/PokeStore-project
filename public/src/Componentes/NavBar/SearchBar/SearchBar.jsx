cc33598cb5d28b5f919dbee814d04fe18d6ff31e
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-07 08:10:34 +0200

    Update SearchBar.jsx

--- public/src/Componentes/NavBar/SearchBar/SearchBar.jsx
+++ public/src/Componentes/NavBar/SearchBar/SearchBar.jsx
@@ -1,53 +1,19 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, IconButton, Input, Button } from '@chakra-ui/react';
-import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
+import { Box, Input, IconButton } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 
 const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const handleSearch = () => {
-    performSearch();
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      performSearch();
-    }
-  };
-
-  const performSearch = () => {
-    const formattedSearchTerm = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1);
-    navigate(`/product/${formattedSearchTerm}`);
-  };
-
-  const toggleSearch = () => {
-    setIsSearchOpen(!isSearchOpen);
-  };
+  const [showInput, setShowInput] = useState(false);
 
   return (
-    <Box className="search-container">
-      <IconButton
-        icon={isSearchOpen ? <CloseIcon /> : <SearchIcon />}
-        aria-label="Toggle Search"
-        onClick={toggleSearch}
-      />
-      {isSearchOpen && (
-        <Box className="search-input-container">
-          <Input
-            type="text"
-            className="search-input"
-            placeholder="Buscar productos..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={handleKeyPress}
-          />
-          <Button className="search-button" onClick={handleSearch}>
-            Buscar
-          </Button>
-        </Box>
+    <Box textAlign="center">
+      {showInput || window.innerWidth >= 768 ? (
+        <Input placeholder="Buscar..." width="200px" />
+      ) : (
+        <IconButton
+          icon={<SearchIcon />}
+          onClick={() => setShowInput(!showInput)}
+        />
       )}
     </Box>
   );
