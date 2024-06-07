7da182fb56dfda6e7e2e21d7395828e8d41bf88b
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-07 17:33:26 +0200

    Update Cart.jsx

--- public/src/Componentes/NavBar/Cart/Cart.jsx
+++ public/src/Componentes/NavBar/Cart/Cart.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import { Box, IconButton, Menu, MenuButton, MenuList, MenuItem, Text, useDisclosure } from '@chakra-ui/react';
-import { FiShoppingCart } from 'react-icons/fi';
-import { useCarrito } from '../../../usecontext/CarritoContext';
+import { Box, Button, Flex, Image, Stack, Text, IconButton } from '@chakra-ui/react';
+import { useCarrito } from '../../usecontext/CarritoContext.jsx';
+import { CloseIcon, AddIcon, MinusIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const { carrito } = useCarrito();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { carrito, eliminar, ajustarCantidad, mensaje } = useCarrito();
+
+  const totalPrecio = carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
 
   return (
-    <Menu isOpen={isOpen}>
-      <MenuButton
-        as={IconButton}
-        icon={<FiShoppingCart />}
-        variant="outline"
-        onMouseEnter={onOpen}
-        onMouseLeave={onClose}
-      />
-      <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
-        {carrito.length === 0 ? (
-          <MenuItem>No hay productos en el carrito.</MenuItem>
-        ) : (
-          carrito.map((producto, index) => (
-            <MenuItem key={index}>
-              <Text>{producto.nombre} - {producto.cantidad}x</Text>
-            </MenuItem>
-          ))
-        )}
-      </MenuList>
-    </Menu>
+    <Box>
+      {mensaje && <Text>{mensaje}</Text>}
+      <Box bg="white" p={4} rounded="md" boxShadow="md">
+        {carrito.map((producto, index) => (
+          <Flex key={index} align="center" justify="space-between" mb={4}>
+            <Image src={producto.imagen} alt={producto.nombre} boxSize="50px" objectFit="cover" mr={4} />
+            <Text flex="1">{producto.nombre}</Text>
+            <Flex align="center">
+              <IconButton
+                icon={<MinusIcon />}
+                onClick={() => ajustarCantidad(producto.id_pokedex, producto.cantidad - 1)}
+              />
+              <Text mx={2}>{producto.cantidad}</Text>
+              <IconButton
+                icon={<AddIcon />}
+                onClick={() => ajustarCantidad(producto.id_pokedex, producto.cantidad + 1)}
+              />
+            </Flex>
+            <IconButton
+              icon={<CloseIcon />}
+              onClick={() => eliminar(producto.id_pokedex)}
+              colorScheme="red"
+              ml={2}
+            />
+          </Flex>
+        ))}
+        <Text>Total: {totalPrecio.toFixed(2)}€</Text>
+        <Button colorScheme="teal" as={Link} to="/carrito">
+          Comprar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
