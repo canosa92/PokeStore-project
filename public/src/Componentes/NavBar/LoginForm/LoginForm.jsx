cc8664c044ba4653c262a7ed4c4157faf9db6578
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-07 08:11:00 +0200

    Update LoginForm.jsx

--- public/src/Componentes/NavBar/LoginForm/LoginForm.jsx
+++ public/src/Componentes/NavBar/LoginForm/LoginForm.jsx
@@ -1,52 +1,32 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useUser } from '../../../usecontext/UserContext.jsx';
-import { Box, Button, Input } from '@chakra-ui/react';
+import React from 'react';
+import { Box, IconButton, Menu, MenuButton, MenuList, MenuItem, Button, useDisclosure } from '@chakra-ui/react';
+import { FiUser } from 'react-icons/fi';
+import { useUser } from '../../../usecontext/UserContext';
 
 const LoginForm = () => {
-  const { setUser } = useUser();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showForm, setShowForm] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:2999/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      if (!response.ok) {
-        throw new Error('Error al iniciar sesión');
-      }
-      const data = await response.json();
-      console.log(data);
-      localStorage.setItem('token', data.user.token);
-      setUser(data.user);
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error);
-    }
-  };
-
-  const toggleForm = () => {
-    setShowForm(!showForm);
-  };
+  const { user, Login, Logout } = useUser();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box onMouseEnter={toggleForm} className="login-form-container">
-      <Button>Login</Button>
-      {showForm && (
-        <form onSubmit={handleSubmit} className="login-form">
-          <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Correo electrónico" />
-          <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" />
-          <Button type="submit">Iniciar sesión</Button>
-          <h4><Link to="/user/register">¿No estás registrado?</Link></h4>
-        </form>
-      )}
-    </Box>
+    <Menu isOpen={isOpen}>
+      <MenuButton
+        as={IconButton}
+        icon={<FiUser />}
+        variant="outline"
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+      />
+      <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
+        {user ? (
+          <>
+            <MenuItem>Welcome, {user.name}</MenuItem>
+            <MenuItem onClick={() => Logout()}>Logout</MenuItem>
+          </>
+        ) : (
+          <MenuItem onClick={() => Login()}>Login</MenuItem>
+        )}
+      </MenuList>
+    </Menu>
   );
 };
 
