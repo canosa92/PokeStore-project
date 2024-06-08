8481d6c3d9b169231e889af1a521b43765501120
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-09 01:13:38 +0200

    Update LoginForm.jsx

--- public/src/Componentes/NavBar/LoginForm/LoginForm.jsx
+++ public/src/Componentes/NavBar/LoginForm/LoginForm.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
-import { Box, Button, FormControl, FormLabel, Input, useDisclosure, VStack, Link, Text, IconButton } from '@chakra-ui/react';
-import { FiUser } from 'react-icons/fi';
-import { useUser } from '../../../usecontext/UserContext';
-import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useUser } from '../../usecontext/UserContext.jsx';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Text,
+  Alert,
+  AlertIcon,
+  Link as ChakraLink,
+} from '@chakra-ui/react';
 
-const LoginForm = () => {
-  const { setUser } = useUser();
+const LoginForm = ({ isOpen, onClose }) => {
+  const { setUser, login } = useUser();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
     try {
       const response = await fetch('http://localhost:2999/user/login', {
         method: 'POST',
@@ -24,79 +37,73 @@ const LoginForm = () => {
         throw new Error('Error al iniciar sesión');
       }
       const data = await response.json();
-      console.log(data);
       localStorage.setItem('token', data.user.token);
       setUser(data.user);
-      onClose();
+      login(data.user, data.user.token);
+      navigate('/myprofile');
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
+      setError(error.message);
     }
+    setIsSubmitting(false);
   };
 
   return (
-    <Box position="relative" onMouseLeave={onClose}>
-      <IconButton
-        icon={<FiUser />}
-        onClick={onOpen}
-        variant="outline"
-        bg="white"
-        _hover={{ bg: "gray.200" }}
-        aria-label="Login"
-      />
-      {isOpen && (
-        <Box
-          position="absolute"
-          top="100%"
-          right={0}
-          mt={2}
-          p={4}
-          bg="gray.800"
-          borderRadius="md"
-          boxShadow="md"
-          zIndex={10}
-          width="300px"
-          onMouseEnter={onOpen}
-        >
-          <form onSubmit={handleSubmit}>
-            <VStack spacing={4}>
-              <FormControl id="email">
-                <FormLabel color="white">Correo electrónico</FormLabel>
-                <Input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Correo electrónico"
-                  color="white"
-                  bg="gray.700"
-                  _placeholder={{ color: 'gray.400' }}
-                  width="100%"
-                />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel color="white">Contraseña</FormLabel>
-                <Input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Contraseña"
-                  color="white"
-                  bg="gray.700"
-                  _placeholder={{ color: 'gray.400' }}
-                  width="100%"
-                />
-              </FormControl>
-              <Button type="submit" colorScheme="blue" width="full">
-                Iniciar sesión
-              </Button>
-              <Text color="white">
-                <Link as={RouterLink} to="/user/register">
-                  ¿No estás registrado?
-                </Link>
-              </Text>
-            </VStack>
-          </form>
-        </Box>
-      )}
+    <Box
+      p={6}
+      bg="gray.700"
+      borderRadius="md"
+      boxShadow="md"
+      mx="auto"
+      width={{ base: '90%', md: 'auto' }} // Ajustamos el ancho en dispositivos móviles
+      position="absolute" // Posición absoluta para centrar verticalmente
+      top="50%" // Posición vertical centrada
+      left="50%" // Posición horizontal centrada
+      transform="translate(-50%, -50%)" // Centrado exacto en el medio
+      display={isOpen ? 'block' : 'none'} // Mostrar solo cuando isOpen es true
+    >
+      <form onSubmit={handleSubmit}>
+        <VStack spacing={4}>
+          <FormControl id="email" isRequired>
+            <FormLabel color="white">Correo electrónico</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Correo electrónico"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel color="white">Contraseña</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Contraseña"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          {error && (
+            <Alert status="error">
+              <AlertIcon />
+              {error}
+            </Alert>
+          )}
+          <Button type="submit" colorScheme="blue" isLoading={isSubmitting} width="100%">
+            Iniciar sesión
+          </Button>
+          <Text color="white">
+            <ChakraLink as={Link} to="/user/register">
+              ¿No estás registrado?
+            </ChakraLink>
+          </Text>
+        </VStack>
+      </form>
     </Box>
   );
 };
