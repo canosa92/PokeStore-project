4874d4fb33343d97e0eb19274f6f111f15446440
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-06-09 01:20:35 +0200

    Update RegisterForm.jsx

--- public/src/Pages/Auht/RegisterForm.jsx
+++ public/src/Pages/Auht/RegisterForm.jsx
@@ -1,31 +1,24 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from '../../usecontext/UserContext.jsx';
-import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  VStack,
-  Text,
-  Alert,
-  AlertIcon,
-} from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, VStack, Text, Alert, AlertIcon, Link as ChakraLink, Image } from '@chakra-ui/react';
+import registrationIllustration from './registrationIllustration.png';
 
-const RegisterForm = ({ role }) => {
+const RegisterForm = ({ isMobile }) => {
   const { user, setUser, register } = useUser();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const rol = user ? user.role : '';
+
   const [formData, setFormData] = useState({
     name: '',
     username: '',
     email: '',
     password: '',
-    role: role,
+    role: rol,
     wishList: [],
-    comments: []
+    comments:[],
   });
 
   const handleChange = (e) => {
@@ -63,76 +56,75 @@ const RegisterForm = ({ role }) => {
   };
 
   return (
-    <Box p={6} bg="gray.700" borderRadius="md" boxShadow="md">
-      {user ? (
-        <Text color="white">No te puedes registrar dos veces</Text>
+    <Box p={6} bg="gray.700" borderRadius="md" boxShadow="md" mx="auto" width={isMobile ? '90%' : 'auto'} textAlign={isMobile ? 'center' : 'left'}>
+      {isMobile ? (
+        <Image src={registrationIllustration} alt="Registration Illustration" mb={4} />
       ) : (
-        <form onSubmit={handleSubmit}>
-          <VStack spacing={4}>
-            <FormControl id="name" isRequired>
-              <FormLabel color="white">Nombre</FormLabel>
-              <Input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Nombre"
-                bg="gray.800"
-                color="white"
-                _placeholder={{ color: 'gray.400' }}
-              />
-            </FormControl>
-            <FormControl id="username" isRequired>
-              <FormLabel color="white">Username</FormLabel>
-              <Input
-                type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                placeholder="Username"
-                bg="gray.800"
-                color="white"
-                _placeholder={{ color: 'gray.400' }}
-              />
-            </FormControl>
-            <FormControl id="email" isRequired>
-              <FormLabel color="white">Correo electrónico</FormLabel>
-              <Input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Correo electrónico"
-                bg="gray.800"
-                color="white"
-                _placeholder={{ color: 'gray.400' }}
-              />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel color="white">Contraseña</FormLabel>
-              <Input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="Contraseña"
-                bg="gray.800"
-                color="white"
-                _placeholder={{ color: 'gray.400' }}
-              />
-            </FormControl>
-            {error && (
-              <Alert status="error">
-                <AlertIcon />
-                {error}
-              </Alert>
-            )}
-            <Button type="submit" colorScheme="blue" isLoading={isSubmitting}>
-              Registrarse
-            </Button>
-          </VStack>
-        </form>
+        <Text fontSize="xl" fontWeight="bold" mb={4}>
+          ¡Regístrate ahora y descubre un mundo de posibilidades!
+        </Text>
       )}
+      <form onSubmit={handleSubmit}>
+        <VStack spacing={4}>
+          <FormControl id="name" isRequired>
+            <FormLabel color="white">Nombre</FormLabel>
+            <Input
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Nombre"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          <FormControl id="username" isRequired>
+            <FormLabel color="white">Usuario</FormLabel>
+            <Input
+              type="text"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Usuario"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          <FormControl id="email" isRequired>
+            <FormLabel color="white">Correo electrónico</FormLabel>
+            <Input
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Correo electrónico"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel color="white">Contraseña</FormLabel>
+            <Input
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Contraseña"
+              bg="gray.800"
+              color="white"
+              _placeholder={{ color: 'gray.400' }}
+            />
+          </FormControl>
+          {error && (
+            <Alert status="error">
+              <AlertIcon />
+              {error}
+            </Alert>
+          )}
+          <Button type="submit" colorScheme="blue" isLoading={isSubmitting} width="100%">
+            Registrarse
+          </Button>
+        </VStack>
+      </form>
     </Box>
   );
 };
